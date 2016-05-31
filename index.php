431964fabba0dc1c9600f36c7e1cd2ac18d0f029
ramity <lcb005@uark.edu>
2016-05-31 11:00:31 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://comp.uark.edu/~lcb005/css/index.css">
+	</head>
+	<body>
+		<div id="header">
+			<div id="headerinr">THE HUB</div>
+		</div>
+		<div id="nav">
+			<div id="navinr">
+				<div class="navitemactive">Home</div>
+				<div class="navitem">Topics</div>
+				<div class="navitem">Information</div>
+				<div class="navitem">Search</div>
+				<div class="navitem">Support us</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<div class="contentitem">
+					<img class="contentitemimage" src="https://placeimg.com/525/350/tech/grayscale">
+					<div class="contentitemtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a metus a purus volutpat tristique. Etiam felis libero, tincidunt quis arcu a, malesuada porttitor lectus. Praesent interdum purus non turpis cursus congue eu eu risus. Ut et fringilla augue. Morbi imperdiet urna sed feugiat eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent vel tincidunt sapien. Aliquam fermentum leo non enim tempor sollicitudin. Nam mattis nulla vitae ligula aliquet gravida. Sed tincidunt blandit ultricies. Duis tincidunt turpis in justo elementum, quis porttitor sem ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed leo lorem, tincidunt ut leo vitae, lacinia bibendum lorem. Fusce sit amet arcu sed est efficitur tincidunt. Suspendisse eu commodo tellus, non lacinia orci. Nam vulputate nibh quis nulla porttitor gravida. Nunc ac lorem id erat maximus pretium. Fusce tincidunt, nisl sit amet suscipit placerat, ex sapien tempus odio, a dignissim nunc enim non elit. Class aptent...</div>
+				</div>
+				<div class="contentitem">
+					<div class="contentitemtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a metus a purus volutpat tristique. Etiam felis libero, tincidunt quis arcu a, malesuada porttitor lectus. Praesent interdum purus non turpis cursus congue eu eu risus. Ut et fringilla augue. Morbi imperdiet urna sed feugiat eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent vel tincidunt sapien. Aliquam fermentum leo non enim tempor sollicitudin. Nam mattis nulla vitae ligula aliquet gravida. Sed tincidunt blandit ultricies. Duis tincidunt turpis in justo elementum, quis porttitor sem ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed leo lorem, tincidunt ut leo vitae, lacinia bibendum lorem. Fusce sit amet arcu sed est efficitur tincidunt. Suspendisse eu commodo tellus, non lacinia orci. Nam vulputate nibh quis nulla porttitor gravida. Nunc ac lorem id erat maximus pretium. Fusce tincidunt, nisl sit amet suscipit placerat, ex sapien tempus odio, a dignissim nunc enim non elit. Class aptent...</div>
+					<img class="contentitemimage" src="https://placeimg.com/525/351/tech/grayscale">
+				</div>
+				<div class="contentitem">
+					<img class="contentitemimage" src="https://placeimg.com/525/352/tech/grayscale">
+					<div class="contentitemtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a metus a purus volutpat tristique. Etiam felis libero, tincidunt quis arcu a, malesuada porttitor lectus. Praesent interdum purus non turpis cursus congue eu eu risus. Ut et fringilla augue. Morbi imperdiet urna sed feugiat eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent vel tincidunt sapien. Aliquam fermentum leo non enim tempor sollicitudin. Nam mattis nulla vitae ligula aliquet gravida. Sed tincidunt blandit ultricies. Duis tincidunt turpis in justo elementum, quis porttitor sem ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed leo lorem, tincidunt ut leo vitae, lacinia bibendum lorem. Fusce sit amet arcu sed est efficitur tincidunt. Suspendisse eu commodo tellus, non lacinia orci. Nam vulputate nibh quis nulla porttitor gravida. Nunc ac lorem id erat maximus pretium. Fusce tincidunt, nisl sit amet suscipit placerat, ex sapien tempus odio, a dignissim nunc enim non elit. Class aptent...</div>
+				</div>
+				<div class="contentitem">
+					<div class="contentitemtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a metus a purus volutpat tristique. Etiam felis libero, tincidunt quis arcu a, malesuada porttitor lectus. Praesent interdum purus non turpis cursus congue eu eu risus. Ut et fringilla augue. Morbi imperdiet urna sed feugiat eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent vel tincidunt sapien. Aliquam fermentum leo non enim tempor sollicitudin. Nam mattis nulla vitae ligula aliquet gravida. Sed tincidunt blandit ultricies. Duis tincidunt turpis in justo elementum, quis porttitor sem ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed leo lorem, tincidunt ut leo vitae, lacinia bibendum lorem. Fusce sit amet arcu sed est efficitur tincidunt. Suspendisse eu commodo tellus, non lacinia orci. Nam vulputate nibh quis nulla porttitor gravida. Nunc ac lorem id erat maximus pretium. Fusce tincidunt, nisl sit amet suscipit placerat, ex sapien tempus odio, a dignissim nunc enim non elit. Class aptent...</div>
+					<img class="contentitemimage" src="https://placeimg.com/525/353/tech/grayscale">
+				</div>
+			</div>
+		</div>
+		<div id="page">
+			<div id="pageinr">
+				<div class="pagebuttonactive">1</div>
+				<div class="pagebutton">2</div>
+				<div class="pagebutton">3</div>
+				<div class="pagebutton">4</div>
+				<div class="pagebutton">5</div>
+				<div class="pagebutton">6</div>
+				<div class="pagebutton">7</div>
+				<div class="pagebutton">8</div>
+				<div class="pagebutton">9</div>
+				<div class="pagebutton">10</div>
+				<div class="pagebutton">11</div>
+				<div class="pagebutton">12</div>
+			</div>
+		</div>
+		<div id="footer">
+			<div id="footerinr">
+				These materials are not endorsed, approved, sponsored, or provided by or on behalf of the University of Arkansas, Fayetteville.
+			</div>
+		</div>
+	</body>
+</html>
